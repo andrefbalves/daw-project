1a6b722cf1ed1da5b8c19906d27b7bef7a48779e
aalves <andrefbster@gmail.com>
2026-05-20 17:56:35 +0100

    first commit

---
+++ settings.py
@@ -0,0 +1,6 @@
+# Modo de debug ativo
+DEBUG = True
+# Porta de execu ção da aplica ção
+PORT = 8080
+# Chave usada para proteger as sess ões da aplica ção
+SECRET_KEY = "secret"
---
+++ views.py
@@ -0,0 +1,4 @@
+from flask import render_template
+
+def home_page():
+    return render_template("index.html")
